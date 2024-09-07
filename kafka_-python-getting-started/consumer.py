a334efb3cbda184e47b57a3dd96f207d4ff6aaca
pear <pearguacamole40zr@gmail.com>
2024-09-08 00:57:26 +0530

    first Commit

---
+++ kafka_-python-getting-started/consumer.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from confluent_kafka import Consumer
+from dotenv import load_dotenv
+import os
+
+if __name__ == '__main__':
+
+    try:
+        load_dotenv()
+        bootstrap_server = os.getenv('bootstrap_server')
+        cluster_api = os.getenv('kafka_key')
+        cluster_pass = os.getenv('kafka_secret')
+    except Exception as e:
+        print(f"error while loading dotenv. Error is {e}")
+
+
+    config = {
+        # User-specific properties that you must set
+        'bootstrap.servers': bootstrap_server,
+        'sasl.username':     cluster_api,
+        'sasl.password':     cluster_pass,
+
+        # Fixed properties
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms':   'PLAIN',
+        'group.id':          'kafka-python-getting-started',
+        'auto.offset.reset': 'earliest'
+    }
+
+    # Create Consumer instance
+    consumer = Consumer(config)
+
+    # Subscribe to topic
+    topic = "purchases"
+    consumer.subscribe([topic])
+
+    # Poll for new messages from Kafka and print them.
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting...")
+            elif msg.error():
+                print("ERROR: %s".format(msg.error()))
+            else:
+                # Extract the (optional) key and value, and print.
+                print("Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
+                    topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
